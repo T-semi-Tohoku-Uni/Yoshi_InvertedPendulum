a2379855eae3cdd16d10c2bb2a0aaca42a476988
yuppy1005 <Y.Y.cricket@outlook.jp>
2023-10-04 15:53:53 +0900

    encoder tested. right one might be broken.

---
+++ Firmware/InvertedPendulum/Debug/Drivers/BNO055_driver-master/bno055_support.d
@@ -0,0 +1,4 @@
+Drivers/BNO055_driver-master/bno055_support.o: \
+ ../Drivers/BNO055_driver-master/bno055_support.c \
+ ../Drivers/BNO055_driver-master/bno055.h
+../Drivers/BNO055_driver-master/bno055.h:
