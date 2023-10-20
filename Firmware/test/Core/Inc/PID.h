4c6a82220857298f1c46c8e2d7e8f99b3c8a1998
yuppy1005 <Y.Y.cricket@outlook.jp>
2023-10-20 23:41:15 +0900

    initial commit PID.h

---
+++ Firmware/test/Core/Inc/PID.h
@@ -0,0 +1,35 @@
+/*
+ * PID.h
+ *
+ *  Created on: Oct 20, 2023
+ *      Author: Yoshi
+ */
+
+#ifndef SRC_PID_H_
+#define SRC_PID_H_
+
+
+typedef struct
+{
+	float Pgain;
+	float Igain;
+	float Dgain;
+	float dt; //ms
+	float Ilimit;
+	float Outlimit;
+}PID_ParamTypeDef;
+
+typedef struct
+{
+	float err;
+	float _err;
+	float Isum;
+
+}PID_ValueTypeDef;
+
+
+float PID_Control(PID_ValueTypeDef *value, PID_ParamTypeDef *param);
+void  PID_Reset(PID_ValueTypeDef *value);
+
+
+#endif /* SRC_PID_H_ */
